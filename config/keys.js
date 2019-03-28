4b3bb101d412493cf742af6dfa00ae0e3449038d
Pranesh <praneshpsg239@gmail.com>
2019-03-28 15:48:48 +0530

    Delete keys

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURL: 'mongodb+srv://praneshpsg239:Qwerty000@myapp-dchog.mongodb.net/test?retryWrites=true',
-    googleClientID: '497438246657-hruide1dthikabj69501a2pfji38g3aj.apps.googleusercontent.com',
-    googleClientScret: '9euebYPsrkdQm-3Uy9hfF9Aq'
-};
